b5d83f492da9dbc20e0726839d8b2593dc505b65
willianns <wilian.nsantos@msn.com>
2020-09-07 23:03:56 -0300

    fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,39 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+//// too fancy, readbility too
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let fizz = i % 3 === 0;
+//         let buzz = i % 5 === 0;
+//         let fizzbuzz = fizz && buzz;
+
+//         if (fizzbuzz)
+//             console.log('fizzbuzz');
+//         else if (fizz)
+//             console.log('fizz');
+//         else if (buzz)
+//             console.log('buzz');
+//         else
+//             console.log(i)
+//     }
+// }
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        let fizz = i % 3 === 0;
+        let buzz = i % 5 === 0;
+        let fizzbuzz = fizz && buzz;
+
+        if (i % 3 === 0 && i % 5 === 0)
+            console.log('fizzbuzz');
+        else if (i % 3 === 0)
+            console.log('fizz');
+        else if (i % 5 === 0)
+            console.log('buzz');
+        else
+            console.log(i)
+    }
+}
 
 module.exports = fizzBuzz;

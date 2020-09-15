0794ee2f5fa30f6488aeb6fe785f8839cc720cc7
willianns <wilian.nsantos@msn.com>
2020-09-14 23:17:01 -0300

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const words = [];
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return words.join(' ');
+} 
+
+// function capitalize(str) {
+//     let result = str[0].toUpperCase();
+
+//     for (let index = 1; index < str.length; index++) {
+//         if (str[index - 1] === ' ') {
+//             result += str[index].toUpperCase();
+//         } else {
+//             result += str[index];
+//         }
+//     }
+
+//     return result;
+// }
 
 module.exports = capitalize;

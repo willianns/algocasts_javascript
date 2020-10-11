3ae612753c2c34bed1dbb6a156ef02c45b8602d2
willianns <wilian.nsantos@msn.com>
2020-10-11 18:08:20 -0300

    fib recursivo

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,51 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// // O(n) linear runtime
+// function fib(n) {
+//     let seq = [0, 1];
+//     for (let i = 2; i <= n; i++) {
+//         seq.push(seq[i-1] + seq[i-2]);
+//     }
+
+//     return seq[n];
+// }
+
+// // O(n) linear runtime (com acumuladores, já que não chamo duas vezes a função, dobrando o processamento)
+// function fib(n, index = 0, acc_l = 0, acc_r = 1) {
+//     if (n <= index) {
+//         return acc_l;        
+//     } else if (n <= 1) {
+//         return n;
+//     }
+
+//     return fib(n, index + 1, acc_r, acc_l + acc_r);
+// }
+
+// // O(2^n) Exponential time - nonono então vamos usar memoization já que usamos ali a chamada duas vezes dobrando o consumo
+function slowFib(n) {
+    if (n < 2) {
+        return n;        
+    } 
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+function memoize(fn) {
+    const cache = {};
+
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;

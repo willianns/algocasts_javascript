628588962c3160dafdae94c390c3e443c5230209
willianns <wilian.nsantos@msn.com>
2020-10-11 21:14:56 -0300

    queue weave

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -24,6 +24,20 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+    let newQueue = new Queue();
+
+    while(sourceOne.peek() || sourceTwo.peek()) {
+        if (sourceOne.peek()) {
+            newQueue.add(sourceOne.remove())
+        }
+
+        if (sourceTwo.peek()) {
+            newQueue.add(sourceTwo.remove())
+        }
+    }
+
+    return newQueue;
+}
 
 module.exports = weave;

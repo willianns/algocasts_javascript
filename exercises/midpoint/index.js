22801756c27da364ee2ee8b0b8ed0c98d82cb715
willianns <wilian.nsantos@msn.com>
2020-10-18 23:08:06 -0300

    midpoint

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,36 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+// function midpoint(list) {
+//     let slow = null;
+//     let fast = null;
+//     for (const node of list) {
+//         if (!slow) {
+//             slow = node;
+//             fast = node.next;
+//             continue;
+//         } 
+        
+//         if (!fast || !fast.next) {
+//             break;
+//         } 
 
+//         fast = fast.next.next;
+//         slow = node;
+//     }
+
+//     return slow;
+// }
+
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+    
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
 module.exports = midpoint;

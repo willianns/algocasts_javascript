d0a41486f08ef38283a7bc57d6f83dbed10b1c66
willianns <wilian.nsantos@msn.com>
2020-10-25 18:43:48 -0300

    events inicial

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,34 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this.events.hasOwnProperty(eventName)) {
+      this.events[eventName] = { handlers: [] };
+    }
+
+    this.events[eventName].handlers.push(callback);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events.hasOwnProperty(eventName)) {
+      this.events[eventName].handlers.forEach(handler => handler());
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    if (this.events.hasOwnProperty(eventName)) {
+      const index = this.events[eventName].handlers.length;
+      this.events[eventName].handlers.splice(0, index);
+    }
+  }
 }
 
 module.exports = Events;
